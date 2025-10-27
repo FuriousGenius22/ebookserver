f79fb16eb76fc6a31a2759e91af3d82c5816ca0b
FuriousGenius22 <dongfangzhizhu123@outlook.com>
2025-10-27 15:27:17 +0000

    feat: cookie rel

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import "express-async-errors";
 import "@/db/connect";
 import express, { ErrorRequestHandler } from "express";
+import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth";
 import { errorHandler } from "./middlewares/error";
 
 const app = express();
 
+// app.use((req, res, next) => {
+//   req.on("data", (chunk) => {
+//     req.body = JSON.parse(chunk);
+//     next();
+//   });
+// });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/auth", authRouter);
-
+app.post("/test", (req, res) => {
+  console.log(req.body);
+  res.json({});
+});
 
 app.use(errorHandler);
 
